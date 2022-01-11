2cc1cb9aed1c4529f1011da2200237507e8635fa
Blain Crawford <blaincrawford@gmail.com>
2022-01-11 10:18:31 -0500

    got the checkbox logic and starting more work on delete and remove logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,10 +7,19 @@ const authorInput = document.querySelector('#author-input');
 const titleInput = document.querySelector('#title-input');
 const pagesInput = document.querySelector('#pages-input');
 const submitButton = document.querySelector('#submit');
+const deleteButton = document.querySelector('#delete-book');
 const removeButton = document.querySelector('#remove-book');
 const haveReadButton = document.querySelector('#have-read');
 let libraryArray = [];
+let bookId = 0;
 
+function Book (id, author, title, numberOfPages, haveRead) {
+  this.id = id;
+  this.author = author;
+  this.title = title;
+  this.numberOfPages = numberOfPages;
+  this.haveRead = haveRead;
+};
 
 const hideBookForm = () => {
   bookForm.classList.add('invisible');
@@ -22,14 +31,52 @@ const showBookForm = () => {
   fullPage.classList.add('darken')
 };
 
-closingButton.addEventListener('click', hideBookForm);
-showBookFormButton.addEventListener('click', showBookForm);
+const haveReadOrNot = () => {
+  if(haveReadButton.checked) {
+    return 'Read!'
+  } else {
+    return 'Still Need to Read!'
+  }
+};
 
-function Book (author, title, numberOfPages, haveRead) {
-  this.auther = author;
-  this.title = title;
-  this.numberOfPages = numberOfPages;
-  this.haveRead = haveRead;
+const clearLibraryBeforeAddingBooks = () => {
+  if (libraryDisplay.hasChildNodes()) {
+    while (libraryDisplay.firstChild) {
+      libraryDisplay.removeChild(libraryDisplay.lastChild);
+    }
+  }
+};
+
+const deleteBookFromLibrary = () => {
+  if (
+    authorInput.value === '' ||
+    titleInput.value === '' ||
+    pagesInput.value === ''
+  ) {
+    alert('ERROR: ALL FIELDS REQUIRED');
+    return;
+  }
+  deleteAuthor = authorInput.value;
+  deleteTitle = titleInput.value;
+  deleteNumberOfPages = pagesInput.value;
+  deleteHaveRead = haveReadOrNot();
+  let checkBook = new Book(deleteAuthor, deleteTitle, deleteNumberOfPages, deleteHaveRead);
+  for(let i = 0; i < libraryArray.length; i++){
+    if(
+      checkBook.author === libraryArray[i].author 
+      // checkBook.title === libraryArray[i].title && 
+      // checkBook.numberOfPages === libraryArray[i].numberOfPages &&
+      // checkBook.haveRead === libraryArray[i].haveRead
+      ) {
+          console.log(libraryArray[i]);
+      }
+  }
+  // clearLibraryBeforeAddingBooks();
+  // addBooksToLibrary();
+  // bookForm.classList.add('invisible');
+  // fullPage.classList.remove('darken');
+  // console.log(checkBook);
+  // console.log(libraryArray);
 };
 
 const addBooksToLibrary = () => {
@@ -53,8 +100,9 @@ const addBooksToLibrary = () => {
     authorDiv.innerHTML = `<h2>Author</h2> <p>${newAuthor}</p>`;
     titleDiv.innerHTML = `<h2>Title</h2> <p>${newTitle}</p>`;
     pagesDiv.innerHTML = `<h2>Number of Pages</h2> <p>${numberOfPages}</p>`;
-    readDiv.innerHTML = `<h2>Read</h2> <p>${haveRead}</p>`;
-    removeButton.innerHTML = `<button class="remove-button">Remove</button>`;
+    readDiv.innerHTML = `<h2>${haveRead}</h2>`;
+    removeButton.innerHTML = `<button id=${bookId} class="remove-button">Remove</button>`;
+    removeButton.addEventListener('click', removeFromLibrary)
     bookContainer.appendChild(authorDiv);
     bookContainer.appendChild(titleDiv);
     bookContainer.appendChild(pagesDiv);
@@ -62,57 +110,50 @@ const addBooksToLibrary = () => {
     bookContainer.appendChild(removeButton);
     libraryDisplay.appendChild(bookContainer);
     bookForm.classList.add('invisible');
-    fullPage.classList.remove('darken')
+    fullPage.classList.remove('darken');
   }
 };
 
 const createBook = () => {
+  let readingStatus = haveReadOrNot();
+  bookId++;
   if (
     authorInput.value === '' ||
     titleInput.value === '' ||
-    pagesInput.value === '' ||
-    !haveReadButton.checked
+    pagesInput.value === ''
   ) {
     alert('ERROR: ALL FIELDS REQUIRED');
     return;
   }
   const newBook = new Book(
+    bookId,
     authorInput.value,
     titleInput.value,
     pagesInput.value,
-    haveReadButton.value
+    readingStatus
   );
   authorInput.value = '';
   titleInput.value = '';
   pagesInput.value = '';
-  // haveReadButton.checked = ;
-  if (libraryDisplay.hasChildNodes()) {
-    while (libraryDisplay.firstChild) {
-      libraryDisplay.removeChild(libraryDisplay.lastChild);
-    }
-  }
+  haveReadButton.checked = false;
+  clearLibraryBeforeAddingBooks();
   libraryArray.push(newBook);
+  addBooksToLibrary();
+  console.log(libraryArray);
+};
+
+const removeFromLibrary = () => {
+  libraryArray.pop();
+  clearLibraryBeforeAddingBooks();
   addBooksToLibrary();
 };
 
-const deleteAccount = () => {
-  libraryArray.pop();
-  if (myDiv.hasChildNodes()) {
-    while (myDiv.firstChild) {
-      myDiv.removeChild(myDiv.lastChild);
-    }
-  }
-  displayUsers();
-};
 
-const haveReadValue = () => {
-  if(haveReadButton.checked) {
-    console.log('checked')
-  } else {
-    console.log('nope')
-  }
-}
-
+/**
+ * adding event listeners to buttons
+ */
 submitButton.addEventListener('click', createBook);
-
-haveReadButton.addEventListener('click', haveReadValue);
+haveReadButton.addEventListener('click', haveReadOrNot);
+closingButton.addEventListener('click', hideBookForm);
+showBookFormButton.addEventListener('click', showBookForm);
+deleteButton.addEventListener('click', deleteBookFromLibrary);
