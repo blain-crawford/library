71dbe9c97a903ac7ee37728bcf575b635d29ffa6
Blain Crawford <blaincrawford@gmail.com>
2022-01-11 13:23:32 -0500

    fully functional now to clean up code and stylize

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,29 +14,44 @@ let libraryArray = [];
 let bookId = 0;
 let buttonId = 0;
 
-function Book (id, author, title, numberOfPages, haveRead) {
+function Book(id, author, title, numberOfPages, haveRead) {
   this.id = id;
   this.author = author;
   this.title = title;
   this.numberOfPages = numberOfPages;
   this.haveRead = haveRead;
+}
+
+const resetBookIdsFrom1 = () => {
+  bookId = 0;
+  for (let j = 0; j < libraryArray.length; j++) {
+    bookId++;
+    libraryArray[j].id = bookId;
+  }
 };
 
 const hideBookForm = () => {
   bookForm.classList.add('invisible');
-  fullPage.classList.remove('darken')
+  fullPage.classList.remove('darken');
 };
 
 const showBookForm = () => {
   bookForm.classList.remove('invisible');
-  fullPage.classList.add('darken')
+  fullPage.classList.add('darken');
+};
+
+const clearBookForm = () => {
+  authorInput.value = '';
+  titleInput.value = '';
+  pagesInput.value = '';
+  haveReadButton.checked = false;
 };
 
 const haveReadOrNot = () => {
-  if(haveReadButton.checked) {
-    return 'Read!'
+  if (haveReadButton.checked) {
+    return 'Read!';
   } else {
-    return 'Still Need to Read!'
+    return 'Still Need to Read!';
   }
 };
 
@@ -62,19 +77,26 @@ const deleteBookFromLibrary = () => {
   deleteNumberOfPages = pagesInput.value;
   deleteHaveRead = haveReadOrNot();
   deleteId = bookId;
-  let checkBook = new Book(deleteId, deleteAuthor, deleteTitle, deleteNumberOfPages, deleteHaveRead);
-  for(let i = 0; i < libraryArray.length; i++){
+  let checkBook = new Book(
+    deleteId,
+    deleteAuthor,
+    deleteTitle,
+    deleteNumberOfPages,
+    deleteHaveRead
+  );
+  for (let i = 0; i < libraryArray.length; i++) {
     let currentBook = libraryArray[i];
-    if(
+    if (
       checkBook.author === currentBook.author &&
-      checkBook.title === currentBook.title && 
-      checkBook.numberOfPages ===currentBook.numberOfPages &&
+      checkBook.title === currentBook.title &&
+      checkBook.numberOfPages === currentBook.numberOfPages &&
       checkBook.haveRead === currentBook.haveRead
-      ) {
-          const index = libraryArray.indexOf(currentBook);
-          libraryArray.splice(index, 1);
-      }
+    ) {
+      const index = libraryArray.indexOf(currentBook);
+      libraryArray.splice(index, 1);
+    }
   }
+  clearBookForm();
   clearLibraryBeforeAddingBooks();
   addBooksToLibrary();
   bookForm.classList.add('invisible');
@@ -82,33 +104,32 @@ const deleteBookFromLibrary = () => {
 };
 
 const addBooksToLibrary = () => {
-  
+  buttonId = 0;
   for (let i = 0; i < libraryArray.length; i++) {
     buttonId++;
-    console.log(buttonId)
-    const bookContainer = document.createElement('div')
+    const bookContainer = document.createElement('div');
     bookContainer.classList.add('book-container');
     const newAuthor = libraryArray[i].author;
     const newTitle = libraryArray[i].title;
     const numberOfPages = libraryArray[i].numberOfPages;
-    const haveRead = libraryArray[i].haveRead
-    const authorDiv = document.createElement('div')
+    const haveRead = libraryArray[i].haveRead;
+    const authorDiv = document.createElement('div');
     authorDiv.classList.add('book-container-line');
-    const titleDiv = document.createElement('div')
+    const titleDiv = document.createElement('div');
     titleDiv.classList.add('book-container-line');
-    const pagesDiv = document.createElement('div')
+    const pagesDiv = document.createElement('div');
     pagesDiv.classList.add('book-container-line');
-    const readDiv = document.createElement('div')
+    const readDiv = document.createElement('div');
     readDiv.classList.add('book-container-line');
-    const removeButton = document.createElement('div')
-    removeButton.classList.add('book-container-remove')
+    const removeButton = document.createElement('div');
+    removeButton.classList.add('book-container-remove');
     removeButton.setAttribute('id', `${buttonId}`);
     authorDiv.innerHTML = `<h2>Author</h2> <p>${newAuthor}</p>`;
     titleDiv.innerHTML = `<h2>Title</h2> <p>${newTitle}</p>`;
     pagesDiv.innerHTML = `<h2>Number of Pages</h2> <p>${numberOfPages}</p>`;
     readDiv.innerHTML = `<h2>${haveRead}</h2>`;
-    removeButton.innerHTML = `<button id=${bookId} class="remove-button">Remove</button>`;
-    removeButton.addEventListener('click', removeFromLibrary)
+    removeButton.innerHTML = `<button id=${buttonId} class='remove-button'>Remove</button>`;
+    removeButton.addEventListener('click', removeFromLibrary);
     bookContainer.appendChild(authorDiv);
     bookContainer.appendChild(titleDiv);
     bookContainer.appendChild(pagesDiv);
@@ -138,10 +159,7 @@ const createBook = () => {
     pagesInput.value,
     readingStatus
   );
-  authorInput.value = '';
-  titleInput.value = '';
-  pagesInput.value = '';
-  haveReadButton.checked = false;
+  clearBookForm();
   clearLibraryBeforeAddingBooks();
   libraryArray.push(newBook);
   buttonId = 0;
@@ -150,24 +168,18 @@ const createBook = () => {
 
 const removeFromLibrary = (e) => {
   let bookIdToRemove = parseInt(e.target.id);
-  for(let i = 0; i < libraryArray.length; i++) {
+  for (let i = 0; i < libraryArray.length; i++) {
     let currentBook = libraryArray[i];
 
-    if(bookIdToRemove === currentBook.id) {
+    if (bookIdToRemove === currentBook.id) {
       let index = libraryArray.indexOf(currentBook);
-      // libraryArray.splice(index, 1);
-      // console.log(currentBook.id);
-      console.log(bookIdToRemove);
-      // console.log(index);
-      // console.log(e.target);
-      // console.log(libraryArray);
-      // console.log(libraryArray);
+      libraryArray.splice(index, 1);
     }
   }
-  // clearLibraryBeforeAddingBooks();
-  // addBooksToLibrary();
+  resetBookIdsFrom1();
+  clearLibraryBeforeAddingBooks();
+  addBooksToLibrary();
 };
-
 
 /**
  * adding event listeners to buttons
